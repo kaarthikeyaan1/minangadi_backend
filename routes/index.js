7091aa7d587537cba5573bebc427852a82f7be7c
RoohiniR2194 <rohinir2194@gmail.com>
2018-12-19 00:04:23 +0530

    Backend server changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var mongo = require('mongodb');
+var assert = require('assert');
 var router = express.Router();
+
+
+var url = "mongodb://localhost:27017";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Minangadi' });
 });
 
 module.exports = router;
